1e0359ca956efc76ae8fc56f5ff03d84d3fd446c
i-naeem <inaeem@protonmail.com>
2022-06-17 00:31:14 +0500

    :sparkles: filter sentences by keywords

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 #! ./Scripts/python.exe
 
-# from utils.keywords_extractor import extract_keywords
-# from utils.text_summarizer import summarize_text
-# from sentence_splitter import SentenceSplitter
+from utils.is_keyword_in_sentence import is_keyword_in_sentence
+from utils.keywords_extractor import extract_keywords
+from utils.text_summarizer import summarize_text
+from sentence_splitter import SentenceSplitter
 from cli import parser
 
 # Step 0: Setup CLI [*]
-# Step 1: Extract Keywords
-# Step 2: Summarize Text
-# Step 3: Filter Extracted Keywords from Summarize Text
-# Step 4: Filter Sentence Where Keywords Appear
+# Step 1: Extract Keywords [*]
+# Step 2: Summarize Text [*]
+# Step 3: Filter Extracted Keywords from Summarize Text [-]
+# Step 4: Filter Sentence Where Keywords Appear [*]
 # Step 5: Replace Keyword with ____ in Sentence
 # Step 6: Find at least 3 Common Words with Keyword
 # Step 7: Create an Multiple Choice Question
 
 args = parser.parse_args()
-
 rfile = args.input
 ofile = args.output
 
 text = rfile.read()
 
-print(text)
+keywords = extract_keywords(text)
+summary = summarize_text(text)
+
+splitter = SentenceSplitter(language="en")
+sentences = splitter.split(text)
+
+sentences_with_keywords = filter(
+    lambda sentence: is_keyword_in_sentence(sentence, keywords),
+    sentences)
