fb3e34d9b48fa6e4eb7f9b2f652729da4fed72a6
i-naeem <inaeem@protonmail.com>
2022-06-16 21:18:06 +0500

    :memo: add roadmap

--- main.py
+++ main.py
@@ -2,8 +2,10 @@
 
 from sentence_splitter import SentenceSplitter
 
-splitter = SentenceSplitter(language="en")
-text = "Hello World. How 0.4 is doing?"
-sentences = splitter.split(text)
-
-print(sentences)
+# Step 1: Extract Keywords
+# Step 2: Summarize Text
+# Step 3: Filter Extracted Keywords from Summarize Text
+# Step 4: Filter Sentence Where Keywords Appear
+# Step 5: Replace Keyword with ____ in Sentence
+# Step 6: Find at least Common Words with Keyword
+# Step 7: Create an Multiple Choice Question
